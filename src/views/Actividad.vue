0be1ae05f6a3e7d7b83432f84859f297cfe47ef1
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2026-05-21 08:42:09 -0500

    Update hallazgos

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -20,7 +20,7 @@ export default {
       tema: 'Microcontroladores y sensores',
       titulo: 'Cuestionario',
       introduccion:
-        '<b> Objetivo:</b> Evaluar la comprensión y el uso adecuado de las herramientas básicas en la ventana <em>Board</em> del <em>software</em> EAGLE.',
+        '<b> <br>Objetivo:</b> Evaluar la comprensión y el uso adecuado de las herramientas básicas en la ventana <em>Board</em> del <em>software</em> EAGLE.',
       barajarPreguntas: true,
       titulo_aprobado: '¡BUEN TRABAJO!',
       titulo_reprobado: 'VUELVA A INTENTARLO.',
